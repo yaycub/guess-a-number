3f41c99f8be690581411e6909d0afa3513043290
Jacob Harrington <jharrington4@gmail.com>
2019-10-02 09:56:10 -0700

    updated app.js function and index.html layout

--- app.js
+++ app.js
@@ -2,23 +2,33 @@
 import compareNumbers from './compare-numbers.js';
 const guess = document.getElementById('guess');
 const submitButton = document.getElementById('submit-button');
+const triesDisplay = document.getElementById('number-of-tries'); 
+const highOrLow = document.getElementById('high-or-low');
 let numberOfTries = 2;
-const correctNumber = 12;
+const correctNumber = Math.floor((Math.random() * 20) + 1);
+triesDisplay.textContent = numberOfTries + 1;
 
 submitButton.addEventListener('click', () => {
     const userInput = guess.value;
-    let upOrDown = compareNumbers(userInput, correctNumber);
-    
+    const sanitizedUserInput = Number(userInput);
+    console.log(correctNumber);
+    let upOrDown = compareNumbers(sanitizedUserInput, correctNumber);
     if (upOrDown === 0){
-        alert('You did it');
+        highOrLow.textContent = 'YOU WINNNNNNNNN YESSSSSSS';
     } else if (upOrDown === 1 && numberOfTries > 0) {
-        alert('Too High');
+        highOrLow.textContent = 'Too High! Try a lower number';
         numberOfTries --;
+        triesDisplay.textContent = numberOfTries + 1;
     } else if (upOrDown === -1 && numberOfTries > 0) {
-        alert('Too low');
+        highOrLow.textContent = 'Too low! Try a higher number';
         numberOfTries --;
-    } else {
-        alert('YOU LOSE :(');
+        triesDisplay.textContent = numberOfTries + 1;
+    } else if (numberOfTries === 0) {
+        highOrLow.textContent = 'You lose :( Refresh and try again!';
         submitButton.disabled = true;
+        triesDisplay.textContent = 0;
     }
 });
+
+
+
