93523d0cec2f3c86dfb0cec8ad3a83e89bf06167
Brittany Houtz <brittany.houtz@gmail.com>
2019-10-04 09:14:30 -0700

    refactored js

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 // Initializing State
 import compareNumbers from './compare-numbers.js';
-const guess = document.getElementById('guess');
+const guessInput = document.getElementById('guess');
 const submitButton = document.getElementById('submit-button');
 const triesDisplay = document.getElementById('number-of-tries'); 
-const highOrLow = document.getElementById('high-or-low');
-let numberOfTries = 2;
+const highOrLowDisplay = document.getElementById('high-or-low');
+let numberOfTriesDisplay = 2;
 const correctNumber = Math.floor((Math.random() * 20) + 1);
-triesDisplay.textContent = numberOfTries + 1;
+triesDisplay.textContent = numberOfTriesDisplay + 1;
 
-// Submit button function
-submitButton.addEventListener('click', () => {
-    // function state
-    const userInput = guess.value;
+const isNumberHighOrLow = () => {
+    const userInput = guessInput.value;
     const sanitizedUserInput = Number(userInput);
-    console.log(correctNumber);
-    let upOrDown = compareNumbers(sanitizedUserInput, correctNumber);
+    let checkResults = compareNumbers(sanitizedUserInput, correctNumber);
     
-    // function run
-    if (upOrDown === 0){
-        highOrLow.textContent = 'YOU WINNNNNNNNN YESSSSSSS';
-    } else if (upOrDown === 1 && numberOfTries > 0) {
-        highOrLow.textContent = 'Too High! Try a lower number';
-        numberOfTries --;
-        triesDisplay.textContent = numberOfTries + 1;
-    } else if (upOrDown === -1 && numberOfTries > 0) {
-        highOrLow.textContent = 'Too low! Try a higher number';
-        numberOfTries --;
-        triesDisplay.textContent = numberOfTries + 1;
-    } else if (numberOfTries === 0) {
-        highOrLow.textContent = 'You lose :( Refresh and try again!';
+    if (checkResults === 0){
+        highOrLowDisplay.textContent = 'YOU WINNNNNNNNN YESSSSSSS';
+    } else if (checkResults === 1 && numberOfTriesDisplay > 0) {
+        highOrLowDisplay.textContent = 'Too High! Try a lower number';
+        numberOfTriesDisplay --;
+        triesDisplay.textContent = numberOfTriesDisplay + 1;
+    } else if (checkResults === -1 && numberOfTriesDisplay > 0) {
+        highOrLowDisplay.textContent = 'Too low! Try a higher number';
+        numberOfTriesDisplay --;
+        triesDisplay.textContent = numberOfTriesDisplay + 1;
+    } else if (numberOfTriesDisplay === 0) {
+        highOrLowDisplay.textContent = 'You lose :( Refresh and try again!';
         submitButton.disabled = true;
         triesDisplay.textContent = 0;
     }
-});
-
+};
+// Submit button function
+submitButton.addEventListener('click', isNumberHighOrLow);
 
 
